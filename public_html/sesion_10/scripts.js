a3162e28cac724787a4432d9f6ebcd99c3f4cc8a
tavo0115 <tavo0115@gmail.com>
2021-09-21 23:31:02 -0500

    Versión_21092021
    Versión 21/09/2021

--- public_html/sesion_10/scripts.js
+++ public_html/sesion_10/scripts.js
@@ -3,5 +3,68 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+var numero1;
+numero1 = 50;
+++numero1;
+var numero2 = 100;
+--numero2;
+var decimal = 13.4;
 
+//alfa +  numericos + _ + $
+
+var ppe01_$alias = 1;
+var ppe01_$alias = 2;
+console.log("Variable " + ppe01_$alias)
+
+let variable_let;
+variable_let = 10;
+console.log("Variable " + variable_let)
+
+const variable_constante = 100;
+console.log("Variable " + variable_constante)
+
+console.log("Variable " + numero1)
+console.log("Variable " + numero2)
+
+var cadena = "Cadena de caracteres";
+
+console.log(cadena + " " + numero1 + numero2);
+console.log(cadena + " " + (numero1 + numero2));
+
+let cadena_caracteres_2 = '';
+
+var cadena_01 = "Cadena de 'Comillas simples'";
+console.log(cadena_01);
+var cadena_02 = 'Cadena de "Comillas dobles"';
+console.log(cadena_02);
+
+var cadena_03 = 'Caracter \n "Salto de línea"';
+console.log(cadena_03);
+var cadena_04 = 'Caracter \t "Tabulador';
+console.log(cadena_04);
+var cadena_05 = 'Caracter \' "Caracter comilla sencilla';
+console.log(cadena_05);
+var cadena_06 = 'Caracter \\ "Diagonal invertida';
+console.log(cadena_06);
+
+var variable_libre = 10;
+console.log(variable_libre);
+
+var variable_libre = "xcxc";
+console.log(variable_libre);
+
+var vector = ['uno', 'dos', 'tres', 'cuatro', 'cinco'];
+console.log(vector[2]);
+
+var boolean = false;
+console.log('Booleano ' + boolean);
+
+if(boolean){
+    console.log("Verdadero");
+}else{
+    console.log("Falso");
+}
+
+/*console.log en el intérprete es igual al alert en netbeans
+ */
 
